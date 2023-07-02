9f284964e68d90abd3ad9cf7f163c257d379a806
Don Tay <dontay0209@gmail.com>
2023-07-02 10:28:09 +0800

    Add env vars validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { DatabaseModule } from './core/database/database.module';
 import { ImageModule } from './modules/image/image.module';
 
@@ -7,6 +8,17 @@ import { ImageModule } from './modules/image/image.module';
   imports: [
     ConfigModule.forRoot({
       envFilePath: `envs/${process.env.NODE_ENV}.env`, // load from file if exists
+      validationSchema: Joi.object({
+        PORT: Joi.number().default(5200),
+        PG_HOST: Joi.string().required(),
+        PG_PORT: Joi.number().required(),
+        PG_USERNAME: Joi.string().required(),
+        PG_PASSWORD: Joi.string().required(),
+        PG_DATABASE: Joi.string().required(),
+        AWS_ACCESS_KEY_ID: Joi.string().required(),
+        AWS_SECRET_ACCESS_KEY: Joi.string().required(),
+        AWS_S3_BUCKET_NAME: Joi.string().required(),
+      }),
     }),
     DatabaseModule.forRootAsync(),
     ImageModule,
